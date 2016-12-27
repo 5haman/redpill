964a2ace655051dadd27d49b090b9e6fdc89688a
Sergey Shyman <sergey@unisend.co>
2016-12-27 15:55:30 +0100

    Working s6-rc

--- Dockerfile
+++ Dockerfile
@@ -8,17 +8,16 @@ ENV VERSION ${VERSION:-0.1}
 ENV SRC_DIR ${SRC_DIR:-/usr/src}
 ENV DOCKER_VERSION ${DOCKER_VERSION:-1.12.5}
 
-RUN apk -U --no-cache add -t .build-deps \
-	bash binutils grep gcc curl git make \
+RUN echo "http://dl-5.alpinelinux.org/alpine/v3.5/main" > $ROOT_DIR/etc/apk/repositories \
+    && echo "http://dl-5.alpinelinux.org/alpine/v3.5/community" >> $ROOT_DIR/etc/apk/repositories \
+    && apk -U --no-cache add -t .build-deps \
+	bash binutils grep gcc curl git make cpio \
 	mkinitfs xorriso musl-dev xz syslinux \
-	perl sed installkernel gmp-dev bc mpfr-dev
-
-RUN apk -U add linux-virtgrsec
-	#open-vm-tools open-vm-tools-grsec \
+	perl sed installkernel gmp-dev bc \
+	mpfr-dev mpc1-dev
 
 WORKDIR "${SRC_DIR}"
 
 VOLUME "${SRC_DIR}"
 
 ENTRYPOINT ["./make_iso.sh"]
-#ENTRYPOINT ["bash"]
--- make_iso.sh
+++ make_iso.sh
@@ -5,55 +5,47 @@ set -eu
 BUILD_DIR="$SRC_DIR/build"
 ISO_DIR="$BUILD_DIR/iso"
 ROOT_DIR="$BUILD_DIR/initrd"
-KERNEL_ID="hyper+"
+KERNEL_VERSION="4.4.39"
+KERNEL_ID="grsec"
 THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 get_deps() {
     # get kernel sources
     if [ ! -d $BUILD_DIR/linux ]; then
-	git clone -b pf-4.4 --depth 1 https://github.com/pfactum/pf-kernel ${BUILD_DIR}/linux
+	#git clone -b pf-4.4 --depth 1 https://github.com/pfactum/pf-kernel ${BUILD_DIR}/linux
+	curl -sSL http://ftp.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_VERSION}.tar.xz | tar xJ -C ${BUILD_DIR}
+	mv ${BUILD_DIR}/linux-${KERNEL_VERSION} ${BUILD_DIR}/linux
+	curl -sSL -o ${BUILD_DIR}/linux/grsecurity.patch  http://dev.alpinelinux.org/~ncopa/grsec/grsecurity-3.1-${KERNEL_VERSION}-201604252206-alpine.patch
+	cd ${BUILD_DIR}/linux
+	patch -p1 -i grsecurity.patch
+    fi
+ 
+    # get docker
+    if [ ! -d $BUILD_DIR/docker ]; then
+        curl -sSL https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz | tar xz -C $BUILD_DIR
     fi
 
-    # get s6 libs
-    if [ ! -d $BUILD_DIR/s6/skalibs ]; then
-        git clone --depth 1 https://github.com/skarnet/skalibs $BUILD_DIR/s6/skalibs
-    fi 
-
-    # get s6 utils
-    for PKG in execline s6 s6-linux-utils s6-portable-utils s6-rc; do
-        if [ ! -d $BUILD_DIR/s6/$PKG ]; then
-            git clone --depth 1 https://github.com/skarnet/$PKG $BUILD_DIR/s6/$PKG
-        fi
-    done 
+    if [ ! -d $BUILD_DIR/tcl/dev ]; then
+	mkdir -p $BUILD_DIR/tcl
+	cd $BUILD_DIR/tcl
+        TCL_REPO_BASE=http://distro.ibiblio.org/tinycorelinux/7.x/x86_64
+        curl -sSL $TCL_REPO_BASE/release/distribution_files/rootfs64.gz | gunzip | cpio -f -i -H newc -d --no-absolute-filenames
+    fi
 }
 
 # copy kernel with needed modules
 build_kernel() {
     cp -vf $SRC_DIR/kernel/config ${BUILD_DIR}/linux/.config
-    cp -vf $SRC_DIR/kernel/Makefile ${BUILD_DIR}/linux/Makefile
 
     # Build kernel
     cd ${BUILD_DIR}/linux
-    make -j${THREADS} oldconfig
-    make -j${THREADS} bzImage
+    #make -j${THREADS} oldconfig
+    #make -j${THREADS} DISABLE_PAX_PLUGINS=y bzImage
+    #make -j${THREADS} DISABLE_PAX_PLUGINS=y modules
     mkdir -p /boot
-    make install
-}
-
-build_s6() {
-    # build s6 libs
-    cd $BUILD_DIR/s6/skalibs
-    ./configure --disable-shared
-    make -j${THREADS}
-    make install
-
-    # build s6 utils
-    for PKG in execline s6 s6-linux-utils s6-portable-utils s6-rc; do
-        cd $BUILD_DIR/s6/$PKG
-        ./configure --enable-static-libc
-        make -j${THREADS}
-	make install
-    done
+    rm -rf /lib/modules/*
+    make install 
+    make modules_install
 }
 
 # make initial fs layout
@@ -62,11 +54,11 @@ prepare_rootfs() {
     mkdir -p $BUILD_DIR $ROOT_DIR $ISO_DIR/boot
 
     # create base rootfs from alpine
-    mkinitfs -F "base" -k -t $ROOT_DIR "$(ls /lib/modules)"
-    rm -rf $ROOT_DIR/lib/modules/*
-
-    cp -v /boot/vmlinuz-$KERNEL_ID $ISO_DIR/boot/vmlinuz
+    mkinitfs -F "base network virtio" -k -t $ROOT_DIR "$(ls /lib/modules)"
     cp -Rv $SRC_DIR/rootfs/* $ROOT_DIR/
+    cp -v /boot/vmlinuz-$KERNEL_ID $ISO_DIR/boot/vmlinuz
+    rm -rf $ROOT_DIR/dev
+    cp -Rv $BUILD_DIR/tcl/dev $ROOT_DIR/dev
 }
 
 install_pkg() {
@@ -74,31 +66,23 @@ install_pkg() {
     echo "http://dl-5.alpinelinux.org/alpine/v3.5/main" > $ROOT_DIR/etc/apk/repositories
     echo "http://dl-5.alpinelinux.org/alpine/v3.5/community" >> $ROOT_DIR/etc/apk/repositories
     apk add --root=$ROOT_DIR -U --initdb --no-cache --allow-untrusted \
-             alpine-baselayout busybox busybox-suid iptables afpfs-ng
-
-    # install_s6
-    for PKG in execline s6 s6-linux-utils s6-portable-utils s6-rc; do
-	cd $BUILD_DIR/s6/$PKG
-        make DESTDIR=$ROOT_DIR install
-    done
+             alpine-baselayout busybox busybox-suid \
+	     s6 s6-rc s6-linux-utils s6-portable-utils s6-networking \
+	     bash iptables afpfs-ng tmux htop dnsmasq
 
     # Install vmware guest additions
     #cp -v /sbin/mount.vmhgfs $ROOT_DIR/sbin/mount.vmhgfs
     #cp -v /usr/sbin/mount.vmhgfs $ROOT_DIR/usr/sbin/mount.vmhgfs
 
     # install_docker
-    if [ ! -d $BUILD_DIR/docker ]; then
-        curl -sSL https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz | tar xz -C $BUILD_DIR
-    fi
     cd $BUILD_DIR/docker
-    cp -v dockerd docker-containerd docker-containerd-shim docker-proxy $ROOT_DIR/usr/bin
+    #cp -v dockerd docker-containerd docker-containerd-shim docker-proxy $ROOT_DIR/usr/bin
 }
 
 clean_rootfs() {
     cd $ROOT_DIR
-    rm -rf usr/lib/s6* usr/lib/execline usr/include/* \
-	   var/cache/apk/* linuxrc usr/share/terminfo/* \
-	   media newroot usr/lib/xtables sbin/xtables-*
+    rm -rf usr/include/* var/cache/apk/* linuxrc usr/share/* \
+	   media newroot .modloop srv lib/libcrypt* etc/mkinitfs etc/*.apk-new
 
     echo "" > $ROOT_DIR/etc/motd
 
@@ -107,12 +91,11 @@ clean_rootfs() {
 
 # Generate final iso image
 make_iso() {
-    #chroot $ROOT_DIR /etc/rc/update
+    chroot $ROOT_DIR /etc/rc.update
 
     # Pack rootfs
     cp -Rv $SRC_DIR/syslinux $ISO_DIR/boot
-    find | cpio -o -H newc | xz --check=none -6 --verbose > $ISO_DIR/boot/initrd.img
-    #find | cpio -o -H newc | xz --check=none -9 -e --verbose > $ISO_DIR/boot/initrd.img
+    find | cpio -o -H newc | xz --check=none -9 -e --verbose > $ISO_DIR/boot/initrd.img
 
     # Builds an image that can be used as an ISO and a disk image
     xorriso  \
@@ -136,7 +119,7 @@ log() {
 log "Get deps sources"       && get_deps
 log "Build optimized kernel" && build_kernel
 log "Prepare initrd dir"     && prepare_rootfs
-log "Build s6 tools"         && build_s6
+#log "Build s6 tools"         && build_s6
 log "Install packages"       && install_pkg
 log "Clean rootfs"           && clean_rootfs
 log "Generate ISO image"     && make_iso
